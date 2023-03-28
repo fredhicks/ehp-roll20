6f45ca740d70910bfd21bf6b2f40c05b573610df
fredhicks <evilhat@gmail.com>
2023-03-28 12:09:08 -0400

    playset support, header mods, icons, expand/collapse

---
+++ Girl By Moonlight/includes/sheet-worker.js
@@ -0,0 +1,32 @@
+var toggles = [
+	"Toggle-Bio-Expand"
+];
+var toggleclicks = "";
+var clicks = 0;
+
+for(var i of toggles) {
+	clicks++;
+	if ( clicks > 1 )
+		toggleclicks = toggleclicks + " ";
+	toggleclicks = toggleclicks + "clicked:" + i;
+}
+
+on(toggleclicks, function(e) {
+	log(e);
+	var id = "";
+	if ( typeof e.sourceAttribute !== "undefined" && e.sourceAttribute !== "" ) {
+		id = e.sourceAttribute;
+	} else {
+		id = (e.htmlAttributes.name).substring(4); // Strips off the 'act_'
+	}
+	log("Toggle Handler: " + id);
+	getAttrs([id], function(a) {
+		if ( a[id] === "1" ) {
+			a[id] = "0";
+		} else {
+			a[id] = "1";
+		}
+		log(a);
+		setAttrs(a);
+	});
+});
