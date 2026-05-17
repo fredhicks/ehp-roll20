3ba2105ed2aa1f432a89d784244ba244d0220e9d
fredhicks <evilhat@gmail.com>
2026-05-17 08:39:15 -0400

    Partial work on Abandoned and Elemental adaptation

--- Monster of the Week/includes/MOTW-sheet-worker.js
+++ Monster of the Week/includes/MOTW-sheet-worker.js
@@ -41,14 +41,17 @@ on("change:startingstats", function() {
 		});
 });
 
-on("sheet:opened change:harm1 change:harm2 change:harm3 change:harm4 change:harm5 change:harm6 change:harm7 change:moves-constructed1 change:constructed-harm1 change:constructed-harm2 change:constructed-harm3 change:imp-dying change:extra-harm1 change:advimp-dying change:extra-harm2 change:extra-okayharm1", function() {
-	const alist = [ "Harm1", "Harm2", "Harm3", "Harm4", "Harm5", "Harm6", "Harm7", "Moves-Constructed1", "Constructed-Harm1", "Constructed-Harm2", "Constructed-Harm3", "Imp-Dying", "Extra-Harm1", "AdvImp-Dying", "Extra-Harm2", "Extra-OkayHarm1", "Imp-OkayHarm" ];
+on("sheet:opened change:skinname change:harm1 change:harm2 change:harm3 change:harm4 change:harm5 change:harm6 change:harm7 change:moves-constructed1 change:constructed-harm1 change:constructed-harm2 change:constructed-harm3 change:imp-dying change:extra-harm1 change:advimp-dying change:extra-harm2 change:extra-okayharm1", function() {
+	const alist = [ "Harm1", "Harm2", "Harm3", "Harm4", "Harm5", "Harm6", "Harm7", "Moves-Constructed1", "Constructed-Harm1", "Constructed-Harm2", "Constructed-Harm3", "Imp-Dying", "Extra-Harm1", "AdvImp-Dying", "Extra-Harm2", "Extra-OkayHarm1", "Imp-OkayHarm", "Elemental-Harm1", "Elemental-Harm2", "SkinName" ];
 	getAttrs(alist,function(a) {
 
 		// Values are "0" if not checked, "on" if checked. First we must adapt for this, as we want them numeric, 0 or 1.
 		for(var i = 0; i < alist.length; i++) {
 			var val = a[alist[i]];
-			if ( val == "on" || val == "1" || val == "true" || val === true ) {
+			if ( val == "41") {
+				// Then it's the Elemental identifier, y'know, so don't mess it up
+				val = 41;
+			} else if ( val == "on" || val == "1" || val == "true" || val === true ) {
 				val = 1;
 			} else {
 				val = 0;
@@ -56,14 +59,22 @@ on("sheet:opened change:harm1 change:harm2 change:harm3 change:harm4 change:harm
 			a[alist[i]] = val;
 		}
 
+		log(a);
+
 		var atts = {};
 		var capacity = 7;
 		var constructed = false; if ( a["Moves-Constructed1"] == 1 ) { constructed = true; capacity += 3; }
 		var dying = false; if ( a["Imp-Dying"] == 1 ) { dying = true; capacity += 1; }
 		var advdying = false; if ( a["AdvImp-Dying"] == 1 ) { advdying = true; capacity += 1; }
 		var okay = false; if ( a["Imp-OkayHarm"] == 1 ) { okay = true; capacity += 1; }
+		log("Elemental Check");
+		log(a["SkinName"]);
+		var elemental = false; if ( a["SkinName"] == 41 ) {
+			elemental = true; capacity += 2;
+		}
 		var taken = a["Harm1"] + a["Harm2"] + a["Harm3"] + a["Harm4"] + a["Harm5"] + a["Harm6"] + a["Harm7"];
 		if ( constructed ) { taken += a["Constructed-Harm1"] + a["Constructed-Harm2"] + a["Constructed-Harm3"]; }
+		if ( elemental ) { taken += a["Elemental-Harm1"] + a["Elemental-Harm2"]; }
 		if ( dying ) { taken += a["Extra-Harm1"]; }
 		if ( okay ) { taken += a["Extra-OkayHarm1"]; }
 		if ( advdying ) { taken += a["Extra-Harm2"]; }
